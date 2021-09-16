f108b6e39ab680534f4ba8a8e5f1f063e99582ae
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-16 10:17:15 -0500

    completed get request

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
-CREATE TABLE "shopping-list" (
+CREATE TABLE "fs-react-shopping" (
     "id" SERIAL PRIMARY KEY,
     "name" varchar(80) not null,
     "quantity" decimal(18,2) not null,
     "unit" varchar(20),
     "purchased" BOOLEAN DEFAULT false
     );
- INSERT INTO "shopping-list"
+ INSERT INTO "fs-react-shopping"
     ("name", "quantity", "unit")
 VALUES
     ('Pinneaple', '3.5545', 'some unit'),
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+const [shoppingList, setShoppingList] = useState([]);
+const getList = () => {
+    axios({
+        method: 'GET',
+        url: '/list',
+    }).then((response) => {
+        console.log(response.data);
+        setShoppingList(response.data);
+    }).catch((error) => {
+        console.log(error);
+        alert('Error making GET request');
+    })
+}
+
+useEffect(() => {
+    getList();
+},[]);
     return (
         <div className="App">
             <Header />
