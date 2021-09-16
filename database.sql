d10372b5c200f3e9cf4370766f044f9d713a1741
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-16 10:29:33 -0500

    get request complete,database name changed.

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
-CREATE TABLE "fs-react-shopping" (
+CREATE TABLE "shopping-list" (
     "id" SERIAL PRIMARY KEY,
     "name" varchar(80) not null,
     "quantity" decimal(18,2) not null,
     "unit" varchar(20),
     "purchased" BOOLEAN DEFAULT false
     );
- INSERT INTO "fs-react-shopping"
+ INSERT INTO "shopping-list"
     ("name", "quantity", "unit")
 VALUES
     ('Pinneaple', '3.5545', 'some unit'),
