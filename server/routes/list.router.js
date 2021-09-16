c0941a67b18e07a0861049ec877ff2ac578e4ca7
Danny Mussie <dannymussie7@gmail.com>
2021-09-16 13:21:20 -0500

    post router complete

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -15,4 +15,26 @@ router.get('/', (req, res) => {
     })
 });
 
+//POST
+router.post('/', (req, res) => {
+    console.log(req.body);
+    let newList = req.body;
+    console.log('adding item to list', newList);
+    const queryText = `
+        INSERT INTO "shopping-list" ("name", "quantity", "unit")
+        VALUES ($1, $2, $3);
+    `;
+    pool.query(queryText, [
+        newList.name,
+        newList.quantity,
+        newList.unit,
+    ]).then((result) => {
+        console.log('POST new list success!');
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error in POST', error);
+        res.sendStatus(500);
+    }); 
+});
+
 module.exports = router;
