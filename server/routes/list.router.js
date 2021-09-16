47994e11587190cd2bfbc042dd1995854d61c1ce
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-16 11:29:21 -0500

    GET router complete.

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * from "shopping-list"';
 
+    pool.query(queryText).then((result) => {
+        console.log("IN GET REQUEST", result.rows);
+        res.send(result.rows);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = router;
