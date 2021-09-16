940ff1cbc324dbea2b62e7c3e9f11995361508ee
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-16 09:35:47 -0500

    Database created.

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+CREATE TABLE "shopping-list" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar(80) not null,
+    "quantity" decimal(18,2) not null,
+    "unit" varchar(20),
+    "purchased" BOOLEAN DEFAULT false
+    );
+ INSERT INTO "shopping-list"
+    ("name", "quantity", "unit")
+VALUES
+    ('Pinneaple', '3.5545', 'some unit'),
+    ('Apples', '16.222', 'some unit')
