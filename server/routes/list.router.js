5f8a74fc5c9e21684a117c3eb3604568a530c91a
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-16 14:00:28 -0500

    put route complete

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -37,4 +37,18 @@ router.post('/', (req, res) => {
     }); 
 });
 
+//PUT
+router.put('/:id', (req,res) =>{
+    console.log('in /list PUT');
+    console.log('req.params is', req.params );
+    const itemId = req.params.id;
+    let queryText =`UPDATE "shopping-list" SET "purchased" = 'true' WHERE id = $1;`;
+    pool.query(queryText, [itemId]).then((result) =>{
+        res.sendStatus(200);
+    }).catch((error) =>{
+        console.log('error with PUT /list');
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
