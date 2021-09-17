57249f5e0efa2d45cc3a3427df1a3bd3cb1b032a
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-17 13:04:23 -0500

    'save' button appends to DOM.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,8 +13,8 @@ function App() {
     let [listArray, setListArray] = useState([]);
 
     //POST
-    const addList = (event) => {
-        event.preventDefault();
+const addList = () => {
+        // event.preventDefault();
         axios({
             method: 'POST',
             url: '/list',
@@ -24,7 +24,7 @@ function App() {
                 unit: listUnit,
             }
         }).then((response) => {
-            console.log(response);
+            console.log('in POST response',response);
             getList();
             setListName('');
             setListQuantity('');
@@ -33,6 +33,7 @@ function App() {
             console.log(error);
         });
     }
+
 
 // renders list on page load
 // linked to getList.then setShoppingList
@@ -67,13 +68,17 @@ useEffect(() => {
                     <p>Item: <input onChange={(event) => setListName(event.target.value)} placeholder="Add item"/></p>
                     <p>Quantity: <input onChange={(event) => setListQuantity(event.target.value)} type="number" placeholder="Quantity"/></p>
                     <p>Unit: <input onChange={(event) => setListUnit(event.target.value)} placeholder="An Absolute Unit" /></p>
-                    <button>Save</button>
+                    <button onClick={addList}>Save</button>
                 </div>
                 <h3>Shopping List</h3>
                 <div>
                     <button>Reset</button>
                     <button>Clear</button>
                     <div className="content-container">
+                    {shoppingList.map(item => 
+                    (<li>{item.quantity}{item.name}{item.unit} </li>)
+                    )}
+                        
                     </div>
                 </div>
             </main>
